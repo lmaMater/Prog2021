b23a8aba6e331a9a1bcdd768561992b8e9dd14be
lmaMater <stepichyarull@gmail.com>
2021-12-24 12:34:06 +0300

    GTests

---
+++ lab2.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+const uint64_t base = 4294967296;
+
+
+typedef struct uint {
+    unsigned int elements[32];
+} uint1024_t;
+
+
+uint1024_t from_uint(unsigned int x) {
+    uint1024_t result;
+
+    result.elements[0] = x;
+    for (int i = 1; i < 32; i++) {
+        result.elements[i] = 0;
+    }
+    return result;
+}
+
+
+uint1024_t add_op(uint1024_t x, uint1024_t y) {
+    uint1024_t result;
+    int buffer = 0;
+
+    for (int i = 0; i < 32; i++) {
+        uint64_t num_x = x.elements[i];
+        uint64_t num_y = y.elements[i];
+        result.elements[i] = (num_x + num_y + buffer) % base;
+        buffer = (num_x + num_y + buffer) / base;
+    }
+    return result;
+}
+
+
+uint1024_t max(uint1024_t x, uint1024_t y) {
+
+    for (int i = 31; i >= 0; i--) {
+        if (x.elements[i] != y.elements[i]) {
+            if (x.elements[i] > y.elements[i]) {
+                return x;
+            }
+            else {
+                return y;
+            }
+        }
+    }
+    return x;
+}
+
+
+int equal(uint1024_t x, uint1024_t y) {
+
+    for (int i = 31; i >= 0; i--) {
+        if (x.elements[i] != y.elements[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+uint1024_t subtr_op(uint1024_t x, uint1024_t y) {
+
+    if (equal(max(x, y), y) == 1) {
+        uint1024_t tmp = x;
+        x = y;
+        y = tmp;
+    }
+
+    uint1024_t result;
+    int buffer = 0;
+    for (int i = 0; i < 32; i++) {
+        uint64_t num_x = x.elements[i];
+        uint64_t num_y = y.elements[i];
+
+        if (num_x - buffer >= num_y) {
+            result.elements[i] = num_x - buffer - num_y;
+            buffer = 0;
+        }
+        else {
+            result.elements[i] = num_x + base - num_y - buffer;
+            buffer = 1;
+        }
+    }
+    return result;
+}
+
+
+uint1024_t mult_op(uint1024_t x, uint1024_t y) {
+    uint1024_t result = from_uint(0);
+
+    for (int i = 0; i < 32; i++) {
+        uint1024_t temp = from_uint(0);
+        uint64_t buffer = 0;
+        uint64_t num_y = y.elements[i];
+        for (int j = 0; j < 32; j++) {
+            uint64_t num_x = x.elements[j];
+            temp.elements[j] = (num_x * num_y + buffer) % base;
+            buffer = (num_x * num_y + buffer) / base;
+        }
+        for (int j = 0; j < i; j++) {
+            for (int q = 31; q > 0; q--) {
+                temp.elements[q] = temp.elements[q - 1];
+            }
+            temp.elements[0] = 0;
+        }
+        result = add_op(result, temp);
+    }
+    return result;
+}
+
+
+void scanf_value(uint1024_t* x, char* line) {
+    uint1024_t y = from_uint(0);
+    for (int i = 0; i < strlen(line); i++) {
+        y = mult_op(y, from_uint(10));
+        y = add_op(y, from_uint(line[i] - '0'));
+    }
+    for (int i = 0; i < 32; i++) {
+        x->elements[i] = y.elements[i];
+    }
+}
+
+
+int printf_value(uint1024_t x, char* ans) {
+    uint1024_t res = from_uint(0);
+    uint1024_t null = from_uint(0);
+    if (memcmp(&x, &null, sizeof(int) * 32) == 0) {
+        ans = "0";
+        return 1;
+    }
+    char line[310];
+    memset(line, 0, 310 * sizeof(char));
+    while (memcmp(&x, &null, sizeof(int) * 32) != 0) {
+        uint64_t buffer = 0;
+        for (int i = 31; i >= 0; i--) {
+            uint64_t num_x = x.elements[i];
+            uint64_t value = (num_x + buffer * base);
+            res.elements[i] = value / 10;
+            buffer = value % 10;
+        }
+        line[strlen(line)] = buffer + '0';
+        x = res;
+    }
+    int j = 0;
+    for (int i = strlen(line) - 1; i >= 0; i--, j++) {
+        char c = line[i];
+        ans[j] = c;
+    }
+    return j;
+}
+
+char* calc(char* n1, char* n2) {
+    uint1024_t x, y;
+    scanf_value(&x, n1);
+    scanf_value(&y, n2);
+
+    char* ans = (char*)calloc(310, sizeof(char));
+    int a = printf_value(add_op(x, y), ans);
+
+    char* out = (char*)calloc(310, sizeof(char));
+    for (int i = 0; i < 310; i++) {
+        out[i] = ans[i];
+        if (i >= a) {
+            out[i] = '\0';
+        }
+    }
+    return out;
+}
