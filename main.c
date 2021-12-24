d92714055be51ab40c6612b9afdac102f11ec17b
asaprocky2011 <57670264+asaprocky2011@users.noreply.github.com>
2021-12-24 06:56:13 +0300

    Update main.c
    изменил код стайл

--- main.c
+++ main.c
@@ -4,27 +4,29 @@
 #include "check.h"
 
 int main(int argc, char *argv[]) {
-    if (checkArgc(argc) == 1) {
+    if (checkArgc(argc) == 1) 
+    {
         return 1;
     }
-
     FILE *input_file = fopen(argv[argc - 1], "r");
-    if (checkFile(input_file) == 1) {
+    if (checkFile(input_file) == 1) 
+    {
         return 1;
     }
-
-    if (strcmp(argv[1], "-l") == 0) {
+    if (strcmp(argv[1], "-l") == 0) 
+    {
         printf("number of lines: %d\n", lines(input_file));
-    } else if (strcmp(argv[1], "-c") == 0) {
+    } else if (strcmp(argv[1], "-c") == 0) 
+    {
         printf("number of bytes: %d\n", bytes(input_file));
-    } else if (strcmp(argv[1], "-w") == 0) {
+    } else if (strcmp(argv[1], "-w") == 0) 
+    {
         printf("number of words: %d\n", words(input_file));
-    } else {
+    } else 
+    {
         wrongOptionWarning();
         return 1;
     }
-
     fclose(input_file);
-
     return 0;
 }
